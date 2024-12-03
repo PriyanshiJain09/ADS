b006dd6ea1895975da82407e7dc4c75cdcb0651c
Priyanshi Jain <145052742+PriyanshiJain09@users.noreply.github.com>
2024-12-03 23:12:21 +0530

    Add files via upload

--- Ques1_Randomized_Quicksort.cpp
+++ Ques1_Randomized_Quicksort.cpp
@@ -1,4 +1,79 @@
 #include <iostream>
+using namespace std;
+
+int comparisons = 0; 
+
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low]; // Use the first element as the pivot
+    int start = low;
+    int end = high;
+
+    while (start < end) {
+        // Increment start while arr[start] <= pivot
+        while (arr[start] <= pivot && start < high) {
+            comparisons++;
+            start++;
+        }
+
+        // Decrement end while arr[end] > pivot
+        while (arr[end] > pivot && end > low) {
+            comparisons++;
+            end--;
+        }
+
+        // Swap elements if start < end
+        if (start < end) {
+            swap(&arr[start], &arr[end]);
+        }
+    }
+
+    // Swap the pivot with arr[end]
+    swap(&arr[low], &arr[end]);
+    return end; // Return the pivot's final position
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high); // Use the modified partition function
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    printArray(arr, n);
+
+    cout << "Number of comparisons: " << comparisons << endl;
+
+    return 0;
+}
+
+
+/*#include <iostream>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
@@ -66,4 +141,4 @@ int main() {
 
     return 0;
 }
-
+*/
--- Ques2_Randomized_Select.cpp
+++ Ques2_Randomized_Select.cpp
@@ -4,6 +4,89 @@
 
 using namespace std;
 
+// Function to swap two elements
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+// Modified partition function based on the provided image
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low]; // The pivot is the first element
+    int start = low;
+    int end = high;
+
+    while (start < end) {
+        // Increment start pointer until arr[start] > pivot
+        while (arr[start] <= pivot && start < high) {
+            start++;
+        }
+        // Decrement end pointer until arr[end] <= pivot
+        while (arr[end] > pivot && end > low) {
+            end--;
+        }
+        // Swap elements if start < end
+        if (start < end) {
+            swap(&arr[start], &arr[end]);
+        }
+    }
+    // Swap the pivot with arr[end]
+    swap(&arr[low], &arr[end]);
+    return end; // Return the pivot's final position
+}
+
+// Randomized partition function
+int partition_r(int arr[], int low, int high) {
+    srand(time(NULL));
+    int random = low + rand() % (high - low + 1); // Random index in range [low, high]
+    swap(&arr[random], &arr[low]); // Swap the random element with the first element (pivot)
+    return partition(arr, low, high);
+}
+
+// Randomized Select function to find the k-th smallest element
+int randomizedSelect(int arr[], int low, int high, int k) {
+    if (low == high) // Base case: only one element
+        return arr[low];
+
+    int pi = partition_r(arr, low, high); // Perform randomized partition
+    int length = pi - low + 1; // Number of elements in the left partition
+
+    if (length == k) // If pivot is the k-th smallest element
+        return arr[pi];
+    else if (k < length) // If k-th element is in the left partition
+        return randomizedSelect(arr, low, pi - 1, k);
+    else // If k-th element is in the right partition
+        return randomizedSelect(arr, pi + 1, high, k - length);
+}
+
+// Main function
+int main() {
+    int n, k;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << "Enter the value of k: ";
+    cin >> k;
+
+    int result = randomizedSelect(arr, 0, n - 1, k);
+    cout << "The " << k << "-th smallest element is: " << result << endl;
+
+    return 0;
+}
+
+
+
+/*#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
 
 void swap(int* a, int* b) {
     int t = *a;
@@ -66,5 +149,5 @@ int main() {
     cout << "The " << k << "-th smallest element is: " << result << endl;
 
     return 0;
-}
+}*/
 
